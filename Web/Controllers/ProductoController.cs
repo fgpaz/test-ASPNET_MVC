6c92e90dbef060ebbb5a926c7243aa7572336a27
Gabriel Paz <gabriel.paz@snoopconsulting.com>
2023-02-08 14:21:14 -0300

    (feat): Producto Table's complete Controller made

---
+++ Web/Controllers/ProductoController.cs
@@ -0,0 +1,20 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CuarzoTest_Paz.Controllers;
+
+public class ProductosController : Controller
+{
+    private readonly CoreLogic _logic;
+
+    public ProductosController(CoreLogic logic)
+    {
+        _logic = logic;
+    }
+
+    public async Task<IActionResult> TablaProductos()
+    {
+        var productos = await _logic.GetAllProductsByCategoria();
+        return View(productos);
+    }
+}
