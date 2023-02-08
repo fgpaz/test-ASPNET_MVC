6eb65d8679c900ba7f95776a9a42468e95880830
Gabriel Paz <gabriel.paz@snoopconsulting.com>
2023-02-07 22:28:22 -0300

    (fix): Database connection now from an Enviromental Variable using appsettings.json and adding Context in Program.cs

---
+++ CuarzoTest_Paz/Program.cs
@@ -0,0 +1,43 @@
+using CuarzoTest_Paz.Models.DB;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// GET Enviromental Variables from appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json", true, true);
+
+// Configuring Database
+
+#region Connection to Database
+
+var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
+builder.Services.AddDbContext<BdcrudTestContext>(options =>
+    options.UseSqlServer(connectionString));
+
+#endregion
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    "default",
+    "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
