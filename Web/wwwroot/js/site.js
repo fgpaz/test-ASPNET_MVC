b9906382bf39d59c0c5500f4283a81420ed2ff1d
Gabriel Paz <gabriel.paz@snoopconsulting.com>
2023-02-08 21:47:33 -0300

    (feat): language set to spanish in modal

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -1,6 +1,24 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+let dtlenguage = {
+    "search": "Buscar: ",
+    "lengthMenu": "Mostrar _MENU_ registros por página",
+    "zeroRecords": "No se encontraron registros",
+    "info": "Mostrando _PAGE_ de _PAGES_ páginas",
+    "paginate": {
+        "previous": "Anterior",
+        "next": "Siguiente"
+    },
+    "select": {
+        rows: {
+            _: "%d filas seleccionadas",
+            1: "1 fila seleccionada"
+        }
+    }
+};
+
+
 function alert(message, type, container) {
     if (!container) container = alertPlaceholder;
     let wrapper = document.createElement('div');
