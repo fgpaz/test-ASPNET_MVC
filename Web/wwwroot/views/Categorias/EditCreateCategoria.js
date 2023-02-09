cada490bd8e5333ed04c4049bbb3c28fc5554956
Gabriel Paz <gabriel.paz@snoopconsulting.com>
2023-02-08 21:51:08 -0300

    (feat): new View for editing and deleting one 'Categoria'

---
+++ Web/wwwroot/views/Categorias/EditCreateCategoria.js
@@ -0,0 +1,26 @@
+$("#mi-lista").select2({
+    theme: "bootstrap",
+});
+
+$(window).resize(function () {
+    $('.select2').css('width', "100%");
+});
+
+$("#btnGuardar").click(function (e) {
+    e.preventDefault();
+    
+    let nombreCategoria = $("#nombreCategoria").val();
+
+    let form = $("#formEditCategoria");
+    let botonGuardar = $("#btnGuardar");
+
+    if (nombreCategoria === "") {
+        showAlert("Debe completar todos los campos obligatorios", "warning");
+        return;
+    }
+
+    botonGuardar.attr("value", "Guardando");
+
+    $("#modalLoader").modal('show');
+    form.submit();
+});
